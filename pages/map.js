1a1bdad07702976648d75e23d43044e9232394b8
SpywarePersues <spywarepersues284@gmail.com>
2022-10-07 11:18:13 +0530

    Minor fix

--- pages/map.js
+++ pages/map.js
@@ -7,7 +7,7 @@ export default function Map(){
                 <iframe src="https://map-zeroday.vercel.app/" className="lg:w-7/12 w-11/12 mx-auto min-h-[550px] rounded-lg"></iframe>
                 <div className="font-Teko text-6xl lg:w-5/12 flex flex-col justify-center">
                     <p className="p-5">Click the button below to access the complete map</p>
-                    <a href="https://map-zeroday.vercel.app/" target={'_blank'} className="w-fit mx-auto button py-3 px-10 rounded-md text-3xl hover:brightness-75 mb-5"><button>Go Now</button></a>
+                    <a href="https://map-zeroday.vercel.app/" target={'_blank'} rel="noreferrer" className="w-fit mx-auto button py-3 px-10 rounded-md text-3xl hover:brightness-75 mb-5"><button>Go Now</button></a>
                 </div>
             </div>
         </ContainerBlock>
