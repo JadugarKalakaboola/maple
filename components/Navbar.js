5dd8a591e35f040bb6c208adb0105c580555fa6b
SpywarePersues <spywarepersues284@gmail.com>
2022-10-07 11:20:38 +0530

    Added creators in navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -7,6 +7,7 @@ export default function Navbar(){
                 <Link href={'./'}><li className="hover:text-gray-100 hover:bg-gray-500 px-4 py-[0.35rem] rounded-md transition-all duration-300 cursor-pointer">Home</li></Link>
                 <Link href={'./map'}><li className="hover:text-gray-100 hover:bg-gray-500 px-4 py-[0.35rem] rounded-md transition-all duration-300 cursor-pointer">Map</li></Link>
                 <Link href={'./features'}><li className="hover:text-gray-100 hover:bg-gray-500 px-4 py-[0.35rem] rounded-md transition-all duration-300 cursor-pointer">Features</li></Link>
+                <Link href={'./creators'}><li className="hover:text-gray-100 hover:bg-gray-500 px-4 py-[0.35rem] rounded-md transition-all duration-300 cursor-pointer">Creators</li></Link>
                 <Link href={'./docs'}><li className="hover:text-gray-100 hover:bg-gray-500 px-4 py-[0.35rem] rounded-md transition-all duration-300 cursor-pointer">Docs</li></Link>
             </ul>
         </div>
