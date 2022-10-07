de3b2b2b79c97df556b452be4b156cb8b7465e98
Siddharth <sid.sharma1704@gmail.com>
2022-10-07 11:05:38 +0530

    Added Map page

--- components/ContainerBlock.js
+++ components/ContainerBlock.js
@@ -4,7 +4,7 @@ import Navbar from "./Navbar";
 
 export default function ContainerBlock({ children }){
     return(
-        <div className="gradient-bg text-gray-100">
+        <div className="gradient-bg min-h-screen text-gray-100">
             <div className="py-5">
                 <Navbar />
                 { children }
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,11 +1,13 @@
+import Link from "next/link"
+
 export default function Navbar(){
     return(
         <div className="py-5">
             <ul className="lg:flex justify-between w-9/12 text-center text-base cursor-pointer lg:w-7/12 mx-auto py-5 text-gray-200 glassmorph rounded-md lg:px-36">
-                <li className="hover:text-gray-400">Home</li>
-                <li className="hover:text-gray-400">Map</li>
-                <li className="hover:text-gray-400">Features</li>
-                <li className="hover:text-gray-400">Docs</li>
+                <Link href={'./'}><li className="hover:text-gray-400">Home</li></Link>
+                <Link href={'./map'}><li className="hover:text-gray-400">Map</li></Link>
+                <Link href={'./features'}><li className="hover:text-gray-400">Features</li></Link>
+                <Link href={'./docs'}><li className="hover:text-gray-400">Docs</li></Link>
             </ul>
         </div>
     )
