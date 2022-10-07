f9136f5dcd6afe155a429498723e11970476c8ab
SpywarePersues <spywarepersues284@gmail.com>
2022-10-07 13:28:35 +0530

    Added creators page

--- pages/creators.js
+++ pages/creators.js
@@ -5,33 +5,54 @@ export default function creators() {
     return (
         <ContainerBlock title="Creators">
             <h1 className='text-5xl text-center pb-5 pt-5 wonderful-text font-semibold'>Creators</h1>
-            <div class="max-w-sm w-full lg:max-w-full lg:flex pt-5 pb-5">
-            <div class="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden" style={{ backgroundImage: '/img/card-left.jpg' }} title="Woman holding a mug">
+            <div class="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-14">
+            <div class="rounded overflow-hidden shadow-lg green-glassmorph">
+            <img class="w-24 block mx-auto my-auto mt-5 mb-2 rounded-full border-solid border-green-600 border-4" src="https://cdn.discordapp.com/attachments/910730837996224584/1027839118350237706/Masterpiece-modified.png" alt="Supriyo" />
+            <div class="px-6 py-4">
+                <div class="font-bold text-xl mb-2 text-center">Supriyo Ghosh</div>
+                <p class="text-gray-300 text-base text-center">
+                <strong>Class :</strong> 7
+                </p>
+                <p class="text-gray-300 text-base text-center">
+                <strong>School :</strong> Army Public School, Delhi Cantt
+                </p>
+                <p class="text-gray-300 text-base text-center">
+                <strong>Discord ID :</strong> SpywarePerseus#4154
+                </p>
             </div>
-            <div class="green-glassmorph rounded-lg p-4 flex flex-col justify-between leading-normal">
-                <div class="mb-8">
-                <div class="flex items-center">
-                <img class="w-14 h-14 rounded-full mr-4 mb-2" src="https://cdn.discordapp.com/attachments/910730837996224584/1027838654208552960/unknown.png" alt="Avatar" />
-                </div>
-                <div class="text-gray-200 font-bold text-xl mb-2">Siddharth Sharma</div>
-                <p class="text-gray-300 text-base w-[58rem]">I am a school student addicted to programming. I like to create your experience at web memorable. Currently I am learning React and wish to become a full stack developer this year.</p>
-                </div>
-            </div>
-            </div>
-
-            <div class="max-w-sm w-full lg:max-w-full lg:flex pt-3 pb-8">
-            <div class="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden" style={{ backgroundImage: '/img/card-left.jpg' }} title="Woman holding a mug">
-            </div>
-            <div class="green-glassmorph rounded-lg p-4 flex flex-col justify-between leading-normal">
-                <div class="mb-8">
-                <div class="flex items-center">
-                <img class="w-14 h-14 rounded-full mr-4 mb-2" src="https://cdn.discordapp.com/attachments/910730837996224584/1027839118350237706/Masterpiece-modified.png" alt="Avatar" />
-                </div>
-                <div class="text-gray-200 font-bold text-xl mb-2">Supriyo Ghosh</div>
-                <p class="text-gray-300 text-base w-[58rem]">I am a school student addicted to programming. I like to create your experience at web memorable. Currently I am learning React and wish to become a full stack developer this year.</p>
-                </div>
+            <div class="px-6 pt-4 pb-2">
+                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#developer</span>
+                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#speaker</span>
+                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#writer</span>
+                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#creator</span>
+                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#helper</span>
+                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#spyware</span>
             </div>
             </div>
+            <div class="rounded overflow-hidden shadow-lg blue-glassmorph">
+            <img class="w-[6.5rem] block mx-auto my-auto mt-5 mb-2 rounded-full border-solid border-red-900 border-4" src="https://cdn.discordapp.com/attachments/910730837996224584/1027838654208552960/unknown.png" alt="Siddharth" />
+            <div class="px-6 py-4">
+                <div class="font-bold text-xl mb-2 text-center">Siddharth Sharma</div>
+                <p class="text-gray-300 text-base text-center">
+                <strong>Class :</strong> 11
+                </p>
+                <p class="text-gray-300 text-base text-center">
+                <strong>School :</strong> Army Public School, Delhi Cantt
+                </p>
+                <p class="text-gray-300 text-base text-center">
+                <strong>Discord ID :</strong> ☁ Cloud ☁#9299
+                </p>
+            </div>
+            <div class="px-6 pt-4 pb-2">
+            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#developer</span>
+            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#designer</span>
+                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#speaker</span>
+                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#writer</span>
+                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#creator</span>
+                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#helper</span>
+            </div>
+            </div>
+        </div>
         </ContainerBlock>
     )
 }
---
+++ pages/features.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ContainerBlock from '../components/ContainerBlock'
+
+export default function features() {
+    return (
+        <ContainerBlock title="Features">
+
+        </ContainerBlock>
+    )
+}
